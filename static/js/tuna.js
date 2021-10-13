4137793176db85ceecee32034e299d0164aa3649
Tomoyuki Tanigaki <87210700+ttanigaki@users.noreply.github.com>
2021-10-14 08:40:53 +0900

    Changes: - use let instead of var - tunacan image file

--- static/js/tuna.js
+++ static/js/tuna.js
@@ -1,19 +1,5 @@
-
-// document.getElementById("tuna").style.display ="none";
-
-var tunacanImg = "/logos/tunacan.png"
-
 cheet('t u n a c a n', function () {
-    console.log("Calling");
-    var logo = document.querySelector('#tuna img');
-    logo.src = tunacanImg
+    let logo = document.querySelector('#tuna img');
+    logo.src = "/images/tkg/tunacan.png"
     logo.outerHTML = `<a href="/tkg/">${logo.outerHTML}</a>`;
-    // document.querySelector('#tuna img').src = tunacanImg
-    // const tuna = document.getElementById("tuna");
-    // if(tuna.style.display=="block"){
-    //     tuna.style.display = "none";
-    // }else{
-    //     tuna.style.display = "block";
-    // }
-    console.log("Called");
 });
